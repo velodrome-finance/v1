ffec16b1c7aef71ec18581ff669f2cb9ab368ea3
coolie <degen.coolie@gmail.com>
2023-03-03 23:15:05 +0000

    feat: Rename token

--- contracts/Velo.sol
+++ contracts/Velo.sol
@@ -5,8 +5,8 @@ import "contracts/interfaces/IVelo.sol";
 
 contract Velo is IVelo {
 
-    string public constant name = "Velodrome";
-    string public constant symbol = "VELO";
+    string public constant name = "Velocimeter";
+    string public constant symbol = "FLOW";
     uint8 public constant decimals = 18;
     uint public totalSupply = 0;
 
