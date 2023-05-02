5a462a0a1b98effce1c3fb898e6c9b65b4bc502a
yakifugu <131336596+yakifugu@users.noreply.github.com>
2023-05-02 00:53:20 -0700

    feat: token sale bonus for ve locker (#10)
    * feat: token sale bonus for ve locker
    
    * add sale status, add lock months to bonus mapping

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,28 +2,53 @@
 pragma solidity 0.8.13;
 
 import "contracts/interfaces/IERC20.sol";
+import "contracts/interfaces/IVotingEscrow.sol";
+
 import "openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 contract TokenSale is Ownable, ReentrancyGuard {
+    // Status of the sale
+    // NOT_STARTED: initial stage, WL sale not started yet
+    // WHITELIST_ROUND: WL sale started, only whitelisted users can participate
+    // PUBLIC_ROUND: public sale started, anyone can participate
+    // FINISHED_UNCLAIMABLE: sale finished, tokens can't be claimed yet
+    // FINISHED_CLAIMABLE: sale finished, tokens can be claimed
+    enum Status { NOT_STARTED, WHITELIST_ROUND, PUBLIC_ROUND, FINISHED_UNCLAIMABLE, FINISHED_CLAIMABLE }
+
     IERC20 public immutable salesToken;
+    IVotingEscrow public immutable ve;
     uint256 public immutable tokensToSell;
     uint256 public immutable wlRate;
     uint256 public immutable publicRate;
+    uint256 public immutable maxBonusPercentage;
 
-    bool public started; // 1st stage: WL sale
-    bool public publicRoundStarted; // 2nd stage: public sale
-    bool public finished; // 3rd stage: finished and claimable
+    Status public status;
     
     bytes32 public merkleRoot;
 
+    // Total tokens sold, not including bonus
+    // After the sale concludes, `tokensToSell - totalTokensSold` amount of tokens will be burned if there are any left
     uint256 public totalTokensSold;
+
+    // Tokens reserved in the contract for sales + max bonus
+    // Only unreserved tokens can be withdrawn by team after sale ends
+    uint256 public reservedTokens;
+
     mapping(address => uint256) public claimableAmounts; // amount of tokens claimable by user
     mapping(address => uint256) public wlCommitments; // amount of ETH committed in WL sale
 
+    uint internal constant MAX_LOCK_WEEKS = 52; // max lock is 1 year
+    uint internal constant MIN_LOCK_WEEKS = 4;
+
+    // mapping from lock time (in months) to bonus percentage
+    // we treat 1 month = 4 weeks for simplicity
+    mapping(uint => uint) internal bonusPercentages; 
+
     constructor(
         IERC20 _salesToken,
+        IVotingEscrow _ve,
         uint256 _wlRate, // whitelist sale ETH to token conversion rate
         uint256 _publicRate, // public sale ETH to token conversion rate
         uint256 _tokensToSell
@@ -36,10 +61,21 @@ contract TokenSale is Ownable, ReentrancyGuard {
         // token must be 18 decimals, otherwise we'll have problems with ETH conversion rate
         require(_salesToken.decimals() == 18, "Token must be 18 decimals");
 
+        require(_ve.token() == address(_salesToken), "ve token address mismatch");
+
         salesToken = _salesToken;
+        ve = _ve;
         tokensToSell = _tokensToSell;
         wlRate = _wlRate;
         publicRate = _publicRate;
+        status = Status.NOT_STARTED;
+        bonusPercentages[1] = 6; // 1 mo lock = 6% bonus in veVS
+        bonusPercentages[2] = 12; // 2 mo lock = 12% bonus
+        bonusPercentages[4] = 18; // 4 mo lock = 18% bonus
+        bonusPercentages[8] = 24; // 8 mo lock = 24% bonus
+        bonusPercentages[12] = 30; // 12 mo lock = 30% bonus
+        maxBonusPercentage = 30; // 30% max bonus
+        _salesToken.approve(address(_ve), type(uint).max);
     }
 
     // owner can set merkle root for WL sale
@@ -47,27 +83,26 @@ contract TokenSale is Ownable, ReentrancyGuard {
         merkleRoot = _merkleRoot;
     }
 
+    // ------- Sales Status Management -------
+
     // start WL sale and transfer tokens to this contract
     function start() external onlyOwner {
-        require(!started, "Already started");
-        started = true;
-        _safeTransferFrom(address(salesToken), msg.sender, address(this), tokensToSell);
+        require(status == Status.NOT_STARTED, "Invalid status");
+        status = Status.WHITELIST_ROUND;
+        _safeTransferFrom(address(salesToken), msg.sender, address(this), tokensToSell * (100 + maxBonusPercentage) / 100);
     }
 
     // start public sale, can only be called after WL sale is started
     function startPublicRound() external onlyOwner {
-        require(started, "Not started yet");
-        require(!publicRoundStarted, "Already started");
-        publicRoundStarted = true;
+        require(status == Status.WHITELIST_ROUND, "Invalid status");
+        status = Status.PUBLIC_ROUND;
     }
     
+    // finish sale, can only be called after public sale is started
+    // ETH will be transferred to owner
     function finish() external onlyOwner {
-        require(!finished, "Already finished");
-        finished = true;
-
-        // transfer remaining tokens back to owner
-        uint256 remainingTokens = salesToken.balanceOf(address(this)) - totalTokensSold;
-        _safeTransfer(address(salesToken), msg.sender, remainingTokens);
+        require(status == Status.PUBLIC_ROUND, "Invalid status");
+        status = Status.FINISHED_UNCLAIMABLE;
 
         // transfer ETH to owner
         uint256 remainingETH = address(this).balance;
@@ -75,14 +110,20 @@ contract TokenSale is Ownable, ReentrancyGuard {
         require(success, "Failed to transfer ether");
     }
 
+    // enable claiming, can only be called after sale is finished
+    function enableClaim() external onlyOwner {
+        require(status == Status.FINISHED_UNCLAIMABLE, "Invalid status");
+        status = Status.FINISHED_CLAIMABLE;
+    }
+    
+    // ------- user interaction -------
+
     // user address + capAmount must match merkle proof
     // capAmount is the max amount of ETH user can commit for WL sale
     function commitWhitelist(uint256 capAmount, bytes32[] calldata merkleProof) external payable nonReentrant {
         require(msg.value > 0, "No ETH sent");
         require(capAmount > 0, "Cap amount must be > 0");
-        require(started, "Not started yet");
-        require(!finished, "Already finished");
-        require(!publicRoundStarted, "Public round already started");
+        require(status == Status.WHITELIST_ROUND, "Not whitelist round");
 
         // Verify the merkle proof
         bytes32 node = keccak256(bytes.concat(keccak256(abi.encode(msg.sender, capAmount))));
@@ -96,25 +137,49 @@ contract TokenSale is Ownable, ReentrancyGuard {
         claimableAmounts[msg.sender] += tokenAmount;
         wlCommitments[msg.sender] += msg.value;
         totalTokensSold += tokenAmount;
+        reservedTokens += ((tokenAmount * (100 + maxBonusPercentage)) / 100);
     }
 
     function commitPublic() external payable nonReentrant {
-        require(publicRoundStarted, "Not started yet");
-        require(!finished, "Already finished");
+        require(status == Status.PUBLIC_ROUND, "Not public round");
 
         uint256 tokenAmount = msg.value * publicRate;
         require(totalTokensSold + tokenAmount <= tokensToSell, "Global cap reached");
         claimableAmounts[msg.sender] += tokenAmount;
         totalTokensSold += tokenAmount;
+        reservedTokens += ((tokenAmount * (100 + maxBonusPercentage)) / 100);
     }
 
     function claim() external nonReentrant {
-        require(finished, "Not finished yet");
+        require(status == Status.FINISHED_CLAIMABLE, "Cannot claim yet");
         require(claimableAmounts[msg.sender] > 0, "Nothing to claim");
 
         uint256 amt = claimableAmounts[msg.sender];
         claimableAmounts[msg.sender] = 0;
         _safeTransfer(address(salesToken), msg.sender, amt);
+
+        // decrease the amount of reserved tokens
+        reservedTokens -= (amt + amt * maxBonusPercentage / 100);
+    }
+
+    function claimAndLock(uint lockMonths) external nonReentrant {
+        require(status == Status.FINISHED_CLAIMABLE, "Cannot claim yet");
+        require(claimableAmounts[msg.sender] > 0, "Nothing to claim");
+        require(bonusPercentages[lockMonths] > 0, "Must lock 1/2/4/8/12 months");
+
+        // convert lock duration to seconds
+        uint256 lockDurationSeconds = lockMonths * 4 weeks;
+
+        uint256 amt = claimableAmounts[msg.sender];
+        claimableAmounts[msg.sender] = 0;
+        ve.create_lock_for(amt, lockDurationSeconds, msg.sender);
+
+        // transfer bonus tokens
+        uint256 bonus = amt * bonusPercentages[lockMonths] / 100;
+        _safeTransfer(address(salesToken), msg.sender, bonus);
+
+        // decrease the amount of reserved tokens
+        reservedTokens -= (amt + amt * maxBonusPercentage / 100);
     }
 
     receive() external payable {}
@@ -129,18 +194,28 @@ contract TokenSale is Ownable, ReentrancyGuard {
         _safeTransfer(address(salesToken), msg.sender, allTokens);
     }
 
+    // use this to withdraw remaining tokens from the contract after sale is finished and claimable (final stage)
+    function withdrawRemainingTokens() external onlyOwner {
+        require(status == Status.FINISHED_CLAIMABLE, "Cannot withdraw remaining tokens yet");
+        _safeTransfer(address(salesToken), msg.sender, getRemainingTokens());
+    }
+
     // View functions
 
-    function getClaimableAmount(address _user) external view returns (uint256) {
+    function getClaimableAmount(address _user) public view returns (uint256) {
         return claimableAmounts[_user];
     }
 
-    function getRemainingTokens() external view returns (uint256) {
-        return tokensToSell - totalTokensSold;
+    function getWlCommitment(address _user) public view returns (uint256) {
+        return wlCommitments[_user];
     }
 
-    function getWlCommitment(address _user) external view returns (uint256) {
-        return wlCommitments[_user];
+    function getRemainingTokens() public view returns (uint256) {
+        return salesToken.balanceOf(address(this)) - reservedTokens;
+    }
+
+    function getStatus() public view returns (uint) {
+        return uint(status);
     }
     
     // Helper functions
--- test/TokenSale.t.sol
+++ test/TokenSale.t.sol
@@ -5,6 +5,7 @@ import './BaseTest.sol';
 contract TokenSaleTest is BaseTest {
 
     TokenSale sale;
+    VotingEscrow ve;
     address user1 = 0x2D66cdD2F86548AaA2B37D7FFbd6aCE28f4D71c4; // WL cap 0.25E
     address user2 = 0xaAA8267C8675Cd632688E726622099D1959797D0; // WL cap 0.25E
     address user3 = 0xF8b3bE51C7D4d1B572b069b182FAE38E04322d6d; // WL cap 0.5E
@@ -14,13 +15,17 @@ contract TokenSaleTest is BaseTest {
         address[] memory owners = new address[](1);
         uint256[] memory amounts = new uint256[](1);
         owners[0] = address(this);
-        amounts[0] = 15000e18;
+        amounts[0] = 20000e18;
         mintVelo(owners, amounts);
+        
+        VeArtProxy artProxy = new VeArtProxy();
+        ve = new VotingEscrow(address(VELO), address(artProxy));
 
         // WL rate 1E = 20000 Token
         // public rate 1E = 10000 Token
         // cap 15000 Token
-        sale = new TokenSale(IERC20(address(VELO)), 20000, 10000, 15000e18);
+        // max 30% bonus
+        sale = new TokenSale(IERC20(address(VELO)), IVotingEscrow(address(ve)), 20000, 10000, 15000e18);
 
         // merkle root is generated in example_proof.json
         sale.setMerkleRoot(0x8d8edd611c4eda08c1a22a6a9b6c3eadc6e4d2e5c7a475268b5be06aaa269de1);
@@ -31,9 +36,16 @@ contract TokenSaleTest is BaseTest {
         vm.deal(user3, 1 ether);
     }
 
-    function testEndToEnd() public {
-        VELO.approve(address(sale), 15000e18);
+    function testNoBonus() public {
+        VELO.approve(address(sale), 19500e18); // approve extra 30% bonus
+
+        // test: status is correct
+        assertEq(sale.getStatus(), 0);
+
         sale.start();
+
+        // test: status is correct
+        assertEq(sale.getStatus(), 1);
 
         // user 1 WL amount = 0.25E
         vm.startPrank(user1);
@@ -77,9 +89,12 @@ contract TokenSaleTest is BaseTest {
 
         sale.startPublicRound();
 
+        // test: status is correct
+        assertEq(sale.getStatus(), 2);
+
         vm.startPrank(user3);
         // test: commitWhitelist should revert
-        vm.expectRevert("Public round already started");
+        vm.expectRevert("Not whitelist round");
         sale.commitWhitelist{value: 0.25 ether}(0.25e18, proof2);
 
         // test: user 3 commits public round
@@ -91,7 +106,7 @@ contract TokenSaleTest is BaseTest {
         sale.commitPublic{value: 0.1 ether}();
 
         // test: cannot claim before end
-        vm.expectRevert("Not finished yet");
+        vm.expectRevert("Cannot claim yet");
         sale.claim();
         vm.stopPrank();
 
@@ -102,6 +117,19 @@ contract TokenSaleTest is BaseTest {
 
         // test: ETH is transferred to owner
         assertEq(balanceAfter - balanceBefore, 1.15 ether);
+        
+        // test: status is correct
+        assertEq(sale.getStatus(), 3);
+
+        // test: still cannot claim before "enableClaim"
+        vm.expectRevert("Cannot claim yet");
+        sale.claim();
+        vm.stopPrank();
+
+        sale.enableClaim();
+        
+        // test: status is correct
+        assertEq(sale.getStatus(), 4);
 
         // test: user1 claims
         vm.prank(user1);
@@ -121,5 +149,99 @@ contract TokenSaleTest is BaseTest {
         // test: cannot claim twice
         vm.expectRevert("Nothing to claim");
         sale.claim();
+        vm.stopPrank();
+
+        balanceBefore = VELO.balanceOf(address(this));
+        sale.withdrawRemainingTokens();
+        balanceAfter = VELO.balanceOf(address(this));
+
+        // test: 30% bonus tokens are returned because no one got bonus 
+        assertEq(balanceAfter - balanceBefore, 15000e18 * 30 / 100);
+
+        // test: totalTokensSold is correct
+        assertEq(sale.totalTokensSold(), 15000e18);
+    }
+
+    function testBonus() public {
+        VELO.approve(address(sale), 19500e18); // approve extra 30% bonus
+        sale.start();
+
+        // user 1 WL amount = 0.25E
+        bytes32[] memory proof1 = new bytes32[](2);
+        proof1[0] = 0x91febd0c2d769895ead0f7873c044f3a367bf2ff9849f6800bc4d2187443cb30;
+        proof1[1] = 0xc0fe84ab9aa5f745f7cc7efa9948f35d0a09665a15e62073e466e8841a593c47;
+        vm.prank(user1);
+        sale.commitWhitelist{value: 0.25 ether}(0.25e18, proof1);
+
+        // test: claimable amount is correct
+        assertEq(sale.getClaimableAmount(user1), 5000e18);
+
+        sale.startPublicRound();
+
+        // user 2 commits public round
+        vm.prank(user2);
+        sale.commitPublic{value: 1 ether}();
+        assertEq(sale.getClaimableAmount(user2), 10000e18);
+
+        // owner calls finish and enable claim
+        uint256 balanceBefore = address(this).balance;
+        sale.finish();
+        uint256 balanceAfter = address(this).balance;
+
+        sale.enableClaim();
+
+        // test: ETH is transferred to owner
+        assertEq(balanceAfter - balanceBefore, 1.25 ether);
+
+        // user1 claims and locks 1 year (12 months)
+        vm.prank(user1);
+        sale.claimAndLock(12);
+
+        // test: 30% liquid token bonus, and 1 veNFT
+        assertEq(VELO.balanceOf(user1), 5000e18 * 30 / 100);
+        assertEq(ve.ownerOf(1), address(user1));
+        assertEq(ve.balanceOf(address(user1)), 1);
+
+        // test: owner can claim remaining
+        balanceBefore = VELO.balanceOf(address(this));
+        sale.withdrawRemainingTokens();
+        balanceAfter = VELO.balanceOf(address(this));
+        uint expectedReturned = 19500e18 - 5000e18 * 130 / 100 - 10000e18 * 130 / 100;
+        assertEq(balanceAfter - balanceBefore, expectedReturned);
+
+        // user2 claims and locks 4 months
+        vm.startPrank(user2);
+
+        // test: cannot lock anything other than 1/2/4/8/12 months
+        vm.expectRevert("Must lock 1/2/4/8/12 months");
+        sale.claimAndLock(13);
+        vm.expectRevert("Must lock 1/2/4/8/12 months");
+        sale.claimAndLock(9);
+        vm.expectRevert("Must lock 1/2/4/8/12 months");
+        sale.claimAndLock(0);
+
+        // lock 4 months
+        sale.claimAndLock(4);
+
+        // test: 18% liquid token bonus, and 1 veNFT
+        assertEq(VELO.balanceOf(user2), 10000e18 * 18 / 100);
+        assertEq(ve.ownerOf(2), address(user2));
+        assertEq(ve.balanceOf(address(user2)), 1);
+
+        // test: cannot claim and lock twice
+        vm.expectRevert("Nothing to claim");
+        sale.claimAndLock(4);
+        vm.stopPrank();
+
+        balanceBefore = VELO.balanceOf(address(this));
+        sale.withdrawRemainingTokens();
+        balanceAfter = VELO.balanceOf(address(this));
+
+        // test: owner can claim remaining
+        expectedReturned = 19500e18 - 5000e18 - 10000e18 - 5000e18 * 30 / 100 - 10000e18 * 18 / 100;
+        assertEq(balanceAfter - balanceBefore, expectedReturned);
+
+        // test: totalTokensSold is correct
+        assertEq(sale.totalTokensSold(), 15000e18);
     }
 }
